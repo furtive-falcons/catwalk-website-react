e486adc55b457c88255f4ed3efe274de095205c8
jackzhen1996 <jackzhen999@gmail.com>
2021-02-26 15:12:23 -0800

    - added subcomponets - added tests for subcomponents

---
+++ client/components/Reviews/components/Ratings.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Ratings = () => {
+    return (
+        <div>
+          ratings
+        </div>
+    )
+}
+
+export default Ratings;
---
+++ client/components/Reviews/components/Reviews.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Reviews = () => {
+    return (
+        <div>
+          reviews
+        </div>
+    )
+}
+
+export default Reviews;
