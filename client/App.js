50827c5942077ba02dcd51438c46115b885eff82
Michael <kllmmc23@gmail.com>
2021-02-27 05:34:20 -0700

    fixed unit test

--- client/App.js
+++ client/App.js
@@ -3,11 +3,9 @@ import Details from './components/Details';
 import Compare from './components/Compare';
 import QnA from './components/QnA';
 import Reviews from './components/Reviews';
-import Header from './components/Header';
 
 const App = () => (
   <div data-test="component-app">
-    <Header />
     <Details />
     <Compare />
     <QnA />
