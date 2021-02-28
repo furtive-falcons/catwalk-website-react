5a8979aa9b5d8859b0b04517aa89750a69f4bab1
Michael Elliott <kllmmc23@gmail.com>
2021-02-28 16:25:22 -0700

    add sliding try1 on banner (#52)
    * add sliding try1 on banner
    
    * add sliding eye strain fix

--- client/components/Paragraph/index.js
+++ client/components/Paragraph/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 
-const Paragraph = ({Paragraph}) => {
+const Paragraph = ({paragraph}) => {
     return (
         <p id="Paragraph">
-            {Paragraph}
+            {paragraph}
         </p>
     )
 }
