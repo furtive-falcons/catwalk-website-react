f18d191eca6a87b5acbcf814ba73d5b82865038b
Michael <kllmmc23@gmail.com>
2021-02-28 15:34:56 -0700

    add sliding try1 on banner

--- client/modules/Details/components/Banner/index.js
+++ client/modules/Details/components/Banner/index.js
@@ -1,15 +1,17 @@
 import React from 'react';
+import Button from '../../../../components/Button';
+import Paragraph from '../../../../components/Paragraph';
+import DivStyles from './styles';
 
-const Banner = () => (
-  <p>
-    site-wide announcement message! - sale / discount
-    <strong>
-      {' '}
-      offer
-    </strong>
-    -
-    <u>new product highlight</u>
-  </p>
-);
+const Banner = () => {
+  const paragraph = `site ${' '} - ${' '} wide announcement message! ${' '}- ${' '} sale / discount`;
+  return (
+    <DivStyles>
+      <Paragraph className="first" paragraph={paragraph} />
+      <span className="first"> &nbsp; offer &nbsp; </span> -
+      <Button className="first" name="new product highlight" />
+    </DivStyles>
+  );
+};
 
 export default Banner;
