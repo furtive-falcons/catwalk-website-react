d315cfc42d7d3bdd075ed4e6506ef600cb417964
Kevin Liu <kevinalexliu@gmail.com>
2021-02-27 22:15:08 -0800

    fix price test

--- client/components/Price/__tests__/Price.js
+++ client/components/Price/__tests__/Price.js
@@ -2,14 +2,14 @@ import React from 'react';
 import Enzyme, { shallow } from 'enzyme';
 import EnzymeAdapter from 'enzyme-adapter-react-16';
 
-import Price from '..';
+import Price from '../index.js';
 
 Enzyme.configure({ adapter: new EnzymeAdapter() });
 
 
 test('It render price', () => {
   const wrapper = shallow(<Price />);
-  const appComponent = wrapper.find('component-price');
-  expect(wrapper).toBeTruthy()
+  const appComponent = wrapper.find("[data-test='component-price']");
+  expect(wrapper).toBeTruthy();
   expect(appComponent.length).toBe(1);
 });
