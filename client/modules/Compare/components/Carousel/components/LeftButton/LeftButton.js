9e531bd8d63c5fb8fa2b19503635e627f46ac0f8
Shay Rosner <shaymrosner@gmail.com>
2021-02-28 16:14:18 -0800

    update left and right card button to use shared component with handler that logs button pressed

--- client/modules/Compare/components/Carousel/components/LeftButton/LeftButton.js
+++ client/modules/Compare/components/Carousel/components/LeftButton/LeftButton.js
@@ -1,10 +1,14 @@
 import React from 'react';
-
+import Button from '../../../../../../components/Button';
+// /Users/shayrosner/Documents/FEC/catwalk-website-react/client/modules/Compare/components/Carousel/components/LeftButton/LeftButton.js
 const LeftButton = () => {
+  const handleOnClick = () => {
+    console.log('Left button was clicked')
+  }
     return (
-      <div>
-        <button>Left Button</button>
-      </div>
+      <>
+        <Button handleOnClick={handleOnClick} name='Left Button'/>
+      </>
     )
 }
 
