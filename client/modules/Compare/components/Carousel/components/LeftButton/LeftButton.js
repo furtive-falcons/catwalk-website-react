9b321ede504d47f40cb38fa0dc7fc853285566c5
Shay Rosner <shaymrosner@gmail.com>
2021-03-01 12:55:05 -0800

    correct eslint errors leaving console logs during developement

--- client/modules/Compare/components/Carousel/components/LeftButton/LeftButton.js
+++ client/modules/Compare/components/Carousel/components/LeftButton/LeftButton.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import Button from '../../../../../../components/Button';
-// /Users/shayrosner/Documents/FEC/catwalk-website-react/client/modules/Compare/components/Carousel/components/LeftButton/LeftButton.js
+
 const LeftButton = () => {
   const handleOnClick = () => {
-    console.log('Left button was clicked')
-  }
-    return (
-      <>
-        <Button handleOnClick={handleOnClick} name='Left Button'/>
-      </>
-    )
-}
+    console.log('Left button was clicked');
+  };
+  return (
+    <>
+      <Button handleOnClick={handleOnClick} name="Left Button" />
+    </>
+  );
+};
 
 export default LeftButton;
