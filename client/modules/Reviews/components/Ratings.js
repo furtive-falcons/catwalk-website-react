14d7942c0d5daa12bdb785ad23ed7bcc0199967b
Jack Zhen <63031182+jackzhen1996@users.noreply.github.com>
2021-03-01 05:06:05 -0800

    Review components overall layout (#61)
    * - added subcomponets
    - added tests for subcomponents
    
    * added subcomponents and tests for subcomponents
    
    * added local styles
    
    * removed comments in test file
    
    * added over css layout for reviews and ratings components
    
    * added overall style for reviews components

--- client/modules/Reviews/components/Ratings.js
+++ client/modules/Reviews/components/Ratings.js
@@ -1,10 +1,19 @@
 import React from 'react'
+import {RatingsContainer} from './styles.js';
+import Score from './ratingsComp/Score.js';
+import Filter from './ratingsComp/Filter.js';
+import Breakdown from './ratingsComp/Breakdown.js';
 
 const Ratings = () => {
     return (
-        <div>
-          ratings
-        </div>
+        <RatingsContainer>
+          {/* score */}
+          <Score/>
+          {/* filter */}
+          <Filter/>
+          {/* breakdown */}
+          <Breakdown/>
+        </RatingsContainer>
     )
 }
 
