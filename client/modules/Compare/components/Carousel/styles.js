7a5192ec4989ea19e5cbca8ce9f0eb2408f36a64
Shay Rosner <shaymrosner@gmail.com>
2021-02-28 20:04:09 -0800

    add demo cards styling

--- client/modules/Compare/components/Carousel/styles.js
+++ client/modules/Compare/components/Carousel/styles.js
@@ -3,21 +3,13 @@ import styled from 'styled-components'
 const CarouselContainer = styled.div`
   flex: 3.5;
   border: 1px solid red;
-  height: 280px;
+  margin: 10px 0px 10px 0px;
+  padding: 10px;
+  height: 300px;
   display: flex;
   flex-direction: row;
   justify-content: space-evenly;
   align-items: center;
 `
-const CardContainer = styled.div`
-  flex: 7;
-  display: flex;
-  border: 1px solid blue;
-  flex-direction: column;
-  justify-content: space-between;
-  align-items: center
-`
 
-
-
-export {CarouselContainer,CardContainer};
+export {CarouselContainer};
