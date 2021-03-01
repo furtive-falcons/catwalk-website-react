4898f7c2dcd89d64afb20fcd4bcffbb03f42d40f
Michael Elliott <kllmmc23@gmail.com>
2021-03-01 16:05:09 -0700

    add refactor (#70)

--- client/components/StyleSelector/index.js
+++ client/components/StyleSelector/index.js
@@ -3,12 +3,19 @@ import Paragraph from '../Paragraph';
 import Button from '../Button';
 
 const StyleSelector = () => {
+  const styles = [1, 2, 3, 4, 5, 6, 7, 8];
+
   const paragraph = 'Style > Selected Style';
   return (
     <>
       <Paragraph>{paragraph}</Paragraph>
-      <Button name="circle" />
-      <i className="fas fa-check" />
+      {styles.map((index) => (
+        <span key={index}>
+          <Button />
+          <i className="fas fa-check" />
+          {index % 4 === 0 ? <br /> : null}
+        </span>
+      ))}
     </>
   );
 };
