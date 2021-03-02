8e3aa32f1b7ffd5f49411771f35b766f595e421a
Michael <kllmmc23@gmail.com>
2021-03-02 11:21:41 -0700

    refactor feat/category

--- client/components/Paragraph/index.js
+++ client/components/Paragraph/index.js
@@ -1,8 +1,19 @@
+import { node, string } from 'prop-types';
 import React from 'react';
 import ParagraphStyles from './styles';
 
-const Paragraph = ({ children }) => (
-  <ParagraphStyles id="Paragraph">{children}</ParagraphStyles>
+const Paragraph = ({ children , className }) => (
+  <ParagraphStyles className={className} id="Paragraph">{children}</ParagraphStyles>
 );
+
+Paragraph.propTypes = {
+  children : node,
+  className : string
+}
+
+Paragraph.defaultProps = {
+  children: null,
+  className : null
+}
 
 export default Paragraph;
--- client/components/ProductDetails/components/ProductInformation/index.js
+++ client/components/ProductDetails/components/ProductInformation/index.js
@@ -2,19 +2,18 @@ import React from 'react';
 import { fontSizeSmaller } from '../../../../ui/ui-fonts';
 import LinkTag from '../../../LinkTag';
 import Price from '../../../Price';
-import ProductCategory from '../../../ProductCategory';
 import ProductTitle from '../../../ProductTitle';
 import StarRating from '../../../StarRating';
-import InformationWrapper from './styles';
+import ProductCategoryStyles from './styles';
 
 const ProducInformation = () => (
-  <InformationWrapper>
+  <>
     <StarRating />
     <LinkTag size={fontSizeSmaller}>Read all reviews</LinkTag>
-    <ProductCategory>category</ProductCategory>
+    <ProductCategoryStyles>category</ProductCategoryStyles>
     <ProductTitle>Expanded Product Name</ProductTitle>
     <Price>$369</Price>
-  </InformationWrapper>
+  </>
 );
 
 export default ProducInformation;
