397fa5824ff4a8bb94c015e76a1cd518c676d850
Shay Rosner <shaymrosner@gmail.com>
2021-03-02 09:17:29 -0800

    pass props down to card assets

--- client/modules/Compare/components/Carousel/components/Card/components/CardAssets/CardAssets.js
+++ client/modules/Compare/components/Carousel/components/Card/components/CardAssets/CardAssets.js
@@ -1,12 +1,23 @@
 import React from 'react';
+// import PropTypes from 'prop-types';
 import Image from './components/Image/Image';
 import FavIcon from './components/FavIcon/FavIcon';
 
-const CardAssets = () => (
+const CardAssets = ({ card }) => (
   <div>
-    <Image />
-    <FavIcon />
+    <img
+      className="default-thumbnail"
+      src={card.thumbnail_url}
+      alt={card.name}
+    />
+    <FavIcon card={card.rating} />
   </div>
 );
+
+// CardAssets.propTypes = {
+//   thumbnail_url: PropTypes.string.isRequired,
+//   name: PropTypes.string.isRequired,
+//   rating: PropTypes.array.isRequired,
+// };
 
 export default CardAssets;
