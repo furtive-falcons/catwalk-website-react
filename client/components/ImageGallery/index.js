1df2040bcc910a417b7e1b875bc221d2b5028f38
Kevin Liu <kevinalexliu@gmail.com>
2021-03-02 10:22:51 -0800

    close tag

--- client/components/ImageGallery/index.js
+++ client/components/ImageGallery/index.js
@@ -1,14 +1,14 @@
 import React from 'react';
-import DivStyles from './styles';
+import DivStyles from './styles'
 
 const imageGallery = () => (
   <DivStyles>
     <div className="expand">
-      <i className="fas fa-expand" />
+      <i className="fas fa-expand"></i>
     </div>
     <div className="arrow">
-      <i className="fas fa-arrow-left" />
-      <i className="fas fa-arrow-right" />
+      <i className="fas fa-arrow-left"></i>
+      <i className="fas fa-arrow-right"></i>
     </div>
   </DivStyles>
 );
