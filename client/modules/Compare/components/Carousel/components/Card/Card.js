dd994ae69bd854909584e38bffac39850009820b
Shay Rosner <shaymrosner@gmail.com>
2021-03-01 19:20:41 -0800

    add dummyData, mapping over each object and pass down props to inner components

--- client/modules/Compare/components/Carousel/components/Card/Card.js
+++ client/modules/Compare/components/Carousel/components/Card/Card.js
@@ -2,22 +2,75 @@ import React from 'react';
 import CardAssets from './components/CardAssets/CardAssets';
 import CardDetails from './components/CardDetails/CardDetails';
 import CardContainer from './styles';
-// client/modules/Compare/components/Carousel/styles.js
+
+const dummyData = [
+  {
+    id: 14931,
+    campus: 'hr-sfo',
+    name: 'Manuela Pants',
+    slogan: 'Nemo ratione deserunt.',
+    description: 'Rerum quia tempore aperiam reiciendis. Eum a enim. Saepe magni tenetur et. Sit est beatae.',
+    category: 'Pants',
+    default_price: '398.00',
+    created_at: '2021-02-23T02:49:03.102Z',
+    updated_at: '2021-02-23T02:49:03.102Z',
+  },
+  {
+    id: 14932,
+    campus: 'hr-sfo',
+    name: 'Elwin Coat',
+    slogan: 'Corrupti eum natus quaerat atque temporibus.',
+    description: 'Molestiae aut ratione. Repellendus aut nesciunt vitae dignissimos. Minus fugit vel tenetur et sunt.',
+    category: 'Coat',
+    default_price: '783.00',
+    created_at: '2021-02-23T02:49:03.102Z',
+    updated_at: '2021-02-23T02:49:03.102Z',
+  },
+  {
+    id: 14034,
+    campus: 'hr-sfo',
+    name: 'Camo Onesie',
+    slogan: 'Blend in to your crowd',
+    description: 'The So Fatigues will wake you up and fit you in. This high energy camo will have you blending in to even the wildest surroundings.',
+    category: 'Jackets',
+    default_price: '140.00',
+    created_at: '2021-02-23T02:49:02.833Z',
+    updated_at: '2021-02-23T02:49:02.833Z',
+  },
+  {
+    id: 14296,
+    campus: 'hr-sfo',
+    name: 'Robin Shirt',
+    slogan: 'Omnis excepturi unde assumenda libero laudantium molestias est nihil.',
+    description: 'Et illum molestiae quia. Deleniti dignissimos sint. Ipsum voluptatem quibusdam amet porro et non doloremque iure.',
+    category: 'Shirt',
+    default_price: '710.00',
+    created_at: '2021-02-23T02:49:03.102Z',
+    updated_at: '2021-02-23T02:49:03.102Z',
+  },
+  {
+    id: 14807,
+    campus: 'hr-sfo',
+    name: 'Emmalee Dress',
+    slogan: 'Et autem est accusantium quo soluta expedita.',
+    description: 'Rerum et ratione sapiente ipsam enim. Ab qui iure aut eaque impedit. Nisi temporibus recusandae eum cupiditate in.',
+    category: 'Dress',
+    default_price: '386.00',
+    created_at: '2021-02-23T02:49:03.102Z',
+    updated_at: '2021-02-23T02:49:03.102Z',
+  },
+];
+
 // map over all cards in related products
 const Card = () => (
   <div>
-    <CardContainer>
-      <CardAssets />
-      <CardDetails />
-    </CardContainer>
-    <CardContainer>
-      <CardAssets />
-      <CardDetails />
-    </CardContainer>
-    <CardContainer>
-      <CardAssets />
-      <CardDetails />
-    </CardContainer>
+    {dummyData.map((card) => (
+      <CardContainer>
+        {console.log('Card card', card)}
+        <CardAssets card={card} key={card.id} />
+        <CardDetails card={card} key={card.id} />
+      </CardContainer>
+    ))}
   </div>
 );
 
--- client/modules/Compare/components/Carousel/components/Card/components/CardDetails/CardDetails.js
+++ client/modules/Compare/components/Carousel/components/Card/components/CardDetails/CardDetails.js
@@ -1,16 +1,27 @@
 import React from 'react';
+// import PropTypes from 'prop-types';
 import Category from './components/Category/Category';
 import ProductName from './components/ProductName/ProductName';
 import Price from './components/Price/Price';
 import AvgRating from './components/AvgRating/AvgRating';
+// import ProductCategory from '../../../../../../../../components/ProductCategory';
+// import Price from '../../../../../../../../components/Price';
+// import ProductTitle from '../../../../../../../../components/ProductTitle';
+// import StarRating from '../../../../../../../../components/StarRating';
 
-const CardDetails = () => (
+const CardDetails = ({ card }) => (
   <div>
-    <Category />
-    <ProductName />
-    <Price />
+    <Category card={card.category} />
+    <ProductName card={card.name} />
+    <Price card={`$${card.default_price}`} />
     <AvgRating />
   </div>
 );
+
+// CardDetails.propTypes = {
+//   default_price: PropTypes.string.isRequired,
+//   name: PropTypes.string.isRequired,
+//   category: PropTypes.string.isRequired,
+// };
 
 export default CardDetails;
