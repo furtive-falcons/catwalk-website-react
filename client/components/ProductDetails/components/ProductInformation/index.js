604807a8abb1378120c76aa6c9f49a5149d03302
Michael <kllmmc23@gmail.com>
2021-03-02 10:33:19 -0700

    rm styles

--- client/components/ProductDetails/components/ProductInformation/index.js
+++ client/components/ProductDetails/components/ProductInformation/index.js
@@ -5,16 +5,16 @@ import Price from '../../../Price';
 import ProductCategory from '../../../ProductCategory';
 import ProductTitle from '../../../ProductTitle';
 import StarRating from '../../../StarRating';
-import InformationWrapper from './styles';
+
 
 const ProducInformation = () => (
-  <InformationWrapper>
+  <>
     <StarRating />
     <LinkTag size={fontSizeSmaller}>Read all reviews</LinkTag>
     <ProductCategory>category</ProductCategory>
     <ProductTitle>Expanded Product Name</ProductTitle>
     <Price>$369</Price>
-  </InformationWrapper>
+  </>
 );
 
 export default ProducInformation;
