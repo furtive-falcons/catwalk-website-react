7431cc549cf623f820bef03e9e72c83757109280
Kevin Liu <kevinalexliu@gmail.com>
2021-03-02 10:20:28 -0800

    fix class to className

--- client/components/ImageGallery/index.js
+++ client/components/ImageGallery/index.js
@@ -1,14 +1,14 @@
 import React from 'react';
-import DivStyles from './styles'
+import DivStyles from './styles';
 
 const imageGallery = () => (
   <DivStyles>
     <div className="expand">
-      <i className="fas fa-expand"></i>
+      <i className="fas fa-expand" />
     </div>
     <div className="arrow">
-      <i class="fas fa-arrow-left"></i>
-      <i class="fas fa-arrow-right"></i>
+      <i className="fas fa-arrow-left" />
+      <i className="fas fa-arrow-right" />
     </div>
   </DivStyles>
 );
