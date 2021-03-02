593e23a19e8c49e2e86c30cb26536655727bb024
Michael <kllmmc23@gmail.com>
2021-03-02 12:31:24 -0700

    refactor style selector add propTypes

--- client/components/Paragraph/styles.js
+++ client/components/Paragraph/styles.js
@@ -2,7 +2,7 @@ import styled from 'styled-components';
 import { fontSizeSmaller } from '../../ui/ui-fonts';
 
 const ParagraphStyles = styled.p`
-  font-size: ${fontSizeSmaller}rem;
+  font-size: ${props => props.size}rem;
 `;
 
 export default ParagraphStyles;
