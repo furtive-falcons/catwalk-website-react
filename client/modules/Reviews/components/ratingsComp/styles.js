661db0afd2872d7964e78c9c6fec3045c102ce31
jackzhen1996 <jackzhen999@gmail.com>
2021-03-02 11:19:05 -0800

    working on ratings components

--- client/modules/Reviews/components/ratingsComp/styles.js
+++ client/modules/Reviews/components/ratingsComp/styles.js
@@ -4,6 +4,26 @@ const ScoreDiv = styled.div`
   width: 85%;
   flex: 1.5;
   border: 1px solid black;
+  display: flex;
+  align-items: center;
+
+  .innerContainer {
+    border: 1px solid black;
+    display: flex;
+    flex-direction: row;
+    width: 75%;
+  }
+
+  .stars {
+    margin-left: 8px;
+    border: 1px solid black;
+    padding-top: 6px;
+
+  }
+
+  h1 {
+    font-size: 35px;
+  }
 `
 const FilterDiv = styled.div`
   width: 85%;
