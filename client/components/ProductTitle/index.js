1550e5d67e1c1bc242bce517ef9fb1b962ceb69f
Michael Elliott <kllmmc23@gmail.com>
2021-03-02 12:16:53 -0700

    refactor title with propTypes and styles (#96)

--- client/components/ProductTitle/index.js
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-
-const ProductTitle = ({ children }) => <h3>{children}</h3>;
-
-export default ProductTitle;
---
+++ client/components/Title/styles.js
@@ -0,0 +1,7 @@
+import styled from 'styled-components';
+
+const StyledTitle = styled.h3(({ size }) => ({
+  fontSize: `${size}rem`,
+}));
+
+export default StyledTitle;
