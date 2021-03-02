690a4c3d85bf908fd2f6920059a316ba65f9f571
Shay Rosner <shaymrosner@gmail.com>
2021-03-01 19:16:43 -0800

    add bolded style to product name

--- client/modules/Compare/components/Carousel/components/Card/components/CardDetails/components/ProductName/ProductName.js
+++ client/modules/Compare/components/Carousel/components/Card/components/CardDetails/components/ProductName/ProductName.js
@@ -1,7 +1,8 @@
 import React from 'react';
 // import PropTypes from 'prop-types';
+import ProductNameBold from './styles';
 
-const ProductName = ({ card }) => <div data-test="component-name">{card}</div>;
+const ProductName = ({ card }) => <ProductNameBold data-test="component-name">{card}</ProductNameBold>;
 
 // ProductName.propTypes = {
 //   card: PropTypes.string.isRequired,
