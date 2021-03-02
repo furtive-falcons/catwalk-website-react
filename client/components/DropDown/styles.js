96916d98b46acbf3796b496300c700664815dc32
Michael <kllmmc23@gmail.com>
2021-03-02 12:43:06 -0700

    refactor dropdown add proptypes

--- client/components/DropDown/styles.js
+++ client/components/DropDown/styles.js
@@ -1,13 +1,11 @@
 import styled from 'styled-components';
 
-const DivStyles = styled.div`
-  width: ${(props) => props.width}rem;
-  & select {
+const StyledSelect = styled.select`
+    width: ${(props) => props.selectSize}rem;
     margin: 0.5rem;
     border: 0.5rem;
     padding: 1rem;
     background-color: purple;
-  }
 `;
 
-export default DivStyles;
+export default StyledSelect;
