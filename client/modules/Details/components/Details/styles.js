3cf451fc37f6d5768785c03cb726a780c0404302
Michael Elliott <kllmmc23@gmail.com>
2021-03-01 18:12:48 -0700

    add max-width 1400px (#77)

--- client/modules/Details/components/Details/styles.js
+++ client/modules/Details/components/Details/styles.js
@@ -10,41 +10,41 @@ import {
 const DivStyles = styled.div`
   .container {
     display: grid;
-    grid-template-rows: repeat(9, minmax(min-content, 8vh));
+    grid-template-rows: repeat(10, minmax(min-content, 10vh));
     grid-template-columns:
       [full-start] 1fr [center-start] repeat(
-        12,
-        [col-start] minmax(min-content, 14rem) [col-end]
+        10,
+        [col-start] minmax(min-content, 9rem) [col-end]
       )
       [center-end] 1fr [full-end];
   }
 
   .gallery {
     background-color: ${colorPrimary};
-    grid-column: center-start / col-end 8;
+    grid-column: center-start / col-end 7;
     grid-row: 1 / 8;
   }
   .info {
     background-color: ${colorSecondary};
-    grid-column: col-end 8 / center-end;
-    grid-row: 1 / 4;
+    grid-column: col-end 7 / center-end;
+    grid-row: 1 / 3;
   }
   .selector {
     background-color: ${colorWhite};
-    grid-column: col-end 8 / center-end;
-    grid-row: 4 / 6;
+    grid-column: col-end 7 / center-end;
+    grid-row: 3 / 5;
   }
   .cart {
     background-color: ${colorGreyLight2};
     color: ${colorWhite};
-    grid-column: col-end 8 / center-end;
-    grid-row: 6 / 8;
+    grid-column: col-end 7 / center-end;
+    grid-row: 5 / 7;
   }
   .footer {
     background-color: ${colorGreyLight1};
     color: ${colorWhite};
     grid-column: center-start / center-end;
-    grid-row: 8 / -1;
+    grid-row: 7 / -1;
   }
 `;
 
