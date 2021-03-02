691f59017969bbe142097f36c5c09a55b4ee22f3
Michael Elliott <kllmmc23@gmail.com>
2021-03-01 17:03:45 -0700

    add components to Product information (#72)

--- client/modules/Details/components/Details/index.js
+++ client/modules/Details/components/Details/index.js
@@ -1,24 +1,26 @@
 import React from 'react';
-import StyleSelector from '../../../../components/StyleSelector';
 import AddToCart from '../../../../components/AddToCart';
+import ProducInformation from '../../../../components/ProductDetails/components/ProductInformation';
+import StyleSelector from '../../../../components/StyleSelector';
+
 import DivStyles from './styles';
 
-const Details = () => {
-  return (
-    <DivStyles data-test="component-details">
-      <div className="container">
-        <div className="gallery">Image Gallery</div>
-        <div className="info">Product Information</div>
-        <div className="selector">
-          <StyleSelector />
-        </div>
-        <div className="cart">
-          <AddToCart />
-        </div>
-        <div className="footer">Footer</div>
+const Details = () => (
+  <DivStyles data-test="component-details">
+    <div className="container">
+      <div className="gallery">Image Gallery</div>
+      <div className="info">
+        <ProducInformation />
       </div>
-    </DivStyles>
-  );
-};
+      <div className="selector">
+        <StyleSelector />
+      </div>
+      <div className="cart">
+        <AddToCart />
+      </div>
+      <div className="footer">Footer</div>
+    </div>
+  </DivStyles>
+);
 
 export default Details;
