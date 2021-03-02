322f1c2123eb74c43c0096b46b99ab729969950b
Michael Elliott <kllmmc23@gmail.com>
2021-03-02 06:57:24 -0700

    add global style changes and refactor (#87)

--- client/components/ProductDetails/index.js
+++ client/components/ProductDetails/index.js
@@ -5,10 +5,18 @@ import ProducInformation from './components/ProductInformation';
 import StyleSelector from '../StyleSelector';
 
 import DivStyles from './styles';
+import Banner from './components/Banner';
+import Header from '../Header';
 
 const ProductDetails = () => (
   <DivStyles data-test="component-details">
     <div className="container">
+      <div className="header">
+        <Header />
+      </div>
+      <div className="banner">
+        <Banner />
+      </div>
       <div className="gallery">
         <ImageGallery />
       </div>
