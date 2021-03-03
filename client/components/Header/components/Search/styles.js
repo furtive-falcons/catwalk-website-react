7502e874eba454ec0c6965ccee7e8c293d745b9a
Michael Elliott <kllmmc23@gmail.com>
2021-03-03 11:28:10 -0700

    add refactor (#116)

--- client/components/Header/components/Search/styles.js
+++ client/components/Header/components/Search/styles.js
@@ -1,10 +1,23 @@
 import styled from 'styled-components';
+import { colorWhite } from '../../../../ui/ui-colors';
+import { fontSizeLarge } from '../../../../ui/ui-fonts';
 
-const SearchStyles = styled.input`
+export const SearchStyles = styled.input`
   background-color: transparent;
   border: none;
-  border-bottom: 1px solid white;
+  border-bottom: 1.5px solid white;
   outline: none;
+  color: ${colorWhite};
+  caret-color: ${colorWhite};
 `;
 
-export default SearchStyles;
+export const SearchWrapper = styled.div`
+  display: flex;
+  gap: 1.6rem;
+  align-items: flex-end;
+`;
+
+export const StyledIcon = styled.i`
+  font-size: ${fontSizeLarge}rem;
+  color: ${colorWhite};
+`;
