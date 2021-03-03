f21bcb5b04b6f809ef87adc16a85993ca28e03d4
Jack Zhen <63031182+jackzhen1996@users.noreply.github.com>
2021-03-03 07:27:20 -0800

    added dummy file (#112)
    * working on ratings components
    
    * created dummy file

--- client/modules/Reviews/components/ratingsComp/Score.js
+++ client/modules/Reviews/components/ratingsComp/Score.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import { ScoreDiv } from './styles.js';
+import Stars from '../../../../components/StarRating';
 
 const Score = () => (
   <ScoreDiv id="score">
-    Average score and stars
+    <div className='innerContainer'>
+      <h1>3.5</h1>
+      <div className='stars'>
+        <Stars/>
+      </div>
+    </div>
   </ScoreDiv>
 );
 
---
+++ client/modules/Reviews/dummy.js
@@ -0,0 +1,110 @@
+const dummy = {
+  product: '14932',
+  page: 0,
+  count: 5,
+  results: [
+    {
+      review_id: 147709,
+      rating: 2,
+      summary: 'Minima error dolorem deserunt qui.',
+      recommend: true,
+      response: null,
+      body: 'Amet excepturi unde. Eos labore velit magni placeat quia cupiditate molestias explicabo et. Consequatur tempora iste accusamus accusamus facilis minus laudantium ex.',
+      date: '2020-05-16T00:00:00.000Z',
+      reviewer_name: 'Juliet15',
+      helpfulness: 29,
+      photos: [
+        {
+          id: 187088,
+          url: 'https://images.unsplash.com/photo-1422557379185-474fa15bf770?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1650&q=80',
+        },
+      ],
+    },
+    {
+      review_id: 147710,
+      rating: 4,
+      summary: 'Itaque tempora sint quos qui.',
+      recommend: true,
+      response: null,
+      body: 'Omnis doloremque voluptas qui aut debitis. Facere blanditiis quis dolore quia debitis praesentium architecto. Consequatur qui aut sit eos rem amet sapiente odit.',
+      date: '2020-12-09T00:00:00.000Z',
+      reviewer_name: 'Germaine89',
+      helpfulness: 27,
+      photos: [
+        {
+          id: 187085,
+          url: 'https://images.unsplash.com/photo-1454024183771-42d54053cd75?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1650&q=80',
+        },
+        {
+          id: 187086,
+          url: 'https://images.unsplash.com/photo-1507920676663-3b72429774ff?ixlib=rb-1.2.1&auto=format&fit=crop&w=1567&q=80',
+        },
+        {
+          id: 187087,
+          url: 'https://images.unsplash.com/photo-1515110371136-7e393289662c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1656&q=80',
+        },
+      ],
+    },
+    {
+      review_id: 147708,
+      rating: 2,
+      summary: 'Consequatur ab omnis veritatis nesciunt blanditiis modi soluta at dolor.',
+      recommend: true,
+      response: null,
+      body: 'Enim incidunt laborum non nulla ipsam consequatur ea assumenda. Mollitia consequatur est. Ad qui praesentium ullam fugit.',
+      date: '2020-03-13T00:00:00.000Z',
+      reviewer_name: 'Jennyfer_Swift',
+      helpfulness: 26,
+      photos: [
+        {
+          id: 187089,
+          url: 'https://images.unsplash.com/photo-1554136920-a1df2909d8f2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1000&q=80',
+        },
+      ],
+    },
+    {
+      review_id: 147706,
+      rating: 2,
+      summary: 'Odio nihil suscipit modi ipsum est autem voluptatem natus.',
+      recommend: true,
+      response: null,
+      body: 'Consectetur laboriosam eaque est dolor dolor pariatur. Velit dignissimos aperiam quia iusto quia tenetur. Consequatur voluptates nobis modi quos. Eum nulla laboriosam pariatur rem in et dignissimos. Accusantium quidem excepturi.',
+      date: '2020-10-12T00:00:00.000Z',
+      reviewer_name: 'Sammie.Leuschke39',
+      helpfulness: 12,
+      photos: [
+        {
+          id: 187092,
+          url: 'https://images.unsplash.com/photo-1554921148-83d8ceda2095?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=668&q=80',
+        },
+        {
+          id: 187093,
+          url: 'https://images.unsplash.com/photo-1551506448-074afa034c05?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=938&q=80',
+        },
+      ],
+    },
+    {
+      review_id: 147707,
+      rating: 2,
+      summary: 'Debitis est voluptates molestias voluptate enim.',
+      recommend: true,
+      response: null,
+      body: 'Ut excepturi sunt minus voluptates id quaerat dolore ad incidunt. Consectetur laboriosam sed voluptas nobis quas animi alias et. Ducimus natus facere aut vel voluptatum consectetur id deleniti quis. Veniam earum rerum ut omnis accusamus. Minus quidem vel maxime. Suscipit corporis ut eos odit eum accusantium quas voluptatibus laborum.',
+      date: '2020-12-19T00:00:00.000Z',
+      reviewer_name: 'Tyrique51',
+      helpfulness: 2,
+      photos: [
+        {
+          id: 187090,
+          url: 'https://images.unsplash.com/photo-1553830591-2f39e38a013c?ixlib=rb-1.2.1&auto=format&fit=crop&w=2760&q=80',
+        },
+        {
+          id: 187091,
+          url: 'https://images.unsplash.com/photo-1519862170344-6cd5e49cb996?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1650&q=80',
+        },
+      ],
+    },
+  ],
+};
+
+export default dummy;
