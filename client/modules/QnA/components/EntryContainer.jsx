f88da5f2128f52faeb3e501be2d06f54f8b370ff
Kevin Liu <kevinalexliu@gmail.com>
2021-03-03 12:00:39 -0800

    sorting base on helpfulness

--- client/modules/QnA/components/EntryContainer.jsx
+++ client/modules/QnA/components/EntryContainer.jsx
@@ -1,11 +1,24 @@
 /* eslint-disable import/extensions */
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import QAEntry from './QAEntry/QAEntry.jsx';
 
-const EntryContainer = ({questions}) => (
-  <div className="entry-container">
-    <QAEntry question={questions[0]} />
-  </div>
-);
+const EntryContainer = ({ questions }) => {
+  const [qns, setQuestions] = useState([]);
+  const sortQuestions = (data) => {
+    const method = 'question_helpfulness';
+    data.sort((a, b) => b[method] - a[method]);
+  };
+
+  useEffect(() => {
+    sortQuestions(questions);
+    setQuestions(questions);
+  }, []);
+
+  return (
+    <div className="entry-container">
+      {qns.map((qn, index) => <QAEntry key={index} question={qn} />)}
+    </div>
+  );
+};
 
 export default EntryContainer;
