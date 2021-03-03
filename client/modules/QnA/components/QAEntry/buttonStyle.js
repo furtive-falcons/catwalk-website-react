79b1694ad23107da51a3410e77bac64970711d37
Kevin Liu <kevinalexliu@gmail.com>
2021-03-02 16:12:16 -0800

    refactor all button with share component (#104)

--- client/modules/QnA/components/QAEntry/buttonStyle.js
+++
@@ -1,9 +0,0 @@
-/* eslint-disable import/extensions */
-import styled from 'styled-components';
-import Button from '../../../../components/Button/index.js';
-
-const AddButton = styled(Button)`
-  border-radius: 0px;
-`;
-
-export default AddButton;
