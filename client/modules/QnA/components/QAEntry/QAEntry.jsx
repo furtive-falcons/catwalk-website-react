c2da67c45f2171e0b35f6f9b5dd3ebb86bfdc76f
Kevin Liu <kevinalexliu@gmail.com>
2021-03-03 13:59:01 -0800

    sorting base on helpfulness (#122)

--- client/modules/QnA/components/QAEntry/QAEntry.jsx
+++ client/modules/QnA/components/QAEntry/QAEntry.jsx
@@ -1,5 +1,5 @@
 /* eslint-disable import/extensions */
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Question from './Question.jsx';
 import AnswerInfo from './AnswerInfo.jsx';
 import Answer from './Answer.jsx';
@@ -8,22 +8,40 @@ import { Entry } from './styles.js';
 import Title from '../Title.jsx';
 import QuestionInfo from './QuestionInfo.jsx';
 
-const QAEntry = ({ question }) => (
-  <Entry className="container">
-    <div className="q">
-      <Title className="q" fontSize="1rem" title="Q:" />
-    </div>
-    <Question body={question.question_body} />
-    <div className="a">
-      <Title fontSize="1rem" title="A:" />
-    </div>
-    <Answer
-      answer={question.answers[68]}
-    />
-    <QuestionInfo question={question} />
-    <AnswerInfo question={question} />
-    <LoadMoreAnswers />
-  </Entry>
-);
+const QAEntry = ({ question }) => {
+  const answers = Object.values(question.answers);
+  const [ans, setAnswers] = useState(answers);
+  const sortAnswers = (data) => {
+    const method = 'helpfulness';
+    data.sort((a, b) => b[method] - a[method]);
+  };
+
+  useEffect(() => {
+    // console.log(answers);
+    sortAnswers(answers);
+    setAnswers(answers);
+  }, []);
+
+  return (
+    <Entry className="container">
+      <div className="q">
+        <Title className="q" fontSize="1rem" title="Q:" />
+      </div>
+      <Question body={question.question_body} />
+      <div className="a">
+        <Title fontSize="1rem" title="A:" />
+      </div>
+      {ans.map((an, index) => (
+        <Answer
+          key={index}
+          answer={an}
+        />
+      ))}
+      <QuestionInfo question={question} />
+      {/* <AnswerInfo question={question} /> */}
+      <LoadMoreAnswers />
+    </Entry>
+  );
+};
 
 export default QAEntry;
