40b3ce1b25ef2d2c4246c69cd6594044919d8071
Kevin Liu <kevinalexliu@gmail.com>
2021-03-03 16:53:14 -0800

    helpful functionality

--- client/modules/QnA/components/QAEntry/QuestionInfo.jsx
+++ client/modules/QnA/components/QAEntry/QuestionInfo.jsx
@@ -1,22 +1,33 @@
 /* eslint-disable import/extensions */
-import React from 'react';
+import React, { useState } from 'react';
 import Helpful from '../../../../components/Helpful';
 import AddAnswer from './AddAnswer.jsx';
 import { StyledSeperator } from './styles.js';
 
-const QuestionInfo = ({ question, handleOnClick }) => (
-  <>
-    <div className="helpful-1">
-      <Helpful
-        helpfulness={question.question_helpfulness}
-        size={1}
-        onClick={() => alert('hello world')}
-        // href="#"
-      />
-    </div>
-    <StyledSeperator />
-    <AddAnswer />
-  </>
-);
+const QuestionInfo = ({ question }) => {
+  const count = question.question_helpfulness;
+  const [yesCount, setYesCount] = useState(count);
+  const [yes, setYes] = useState(false);
+  const handleOnClick = (e) => {
+    e.preventDefault();
+    if (!yes) {
+      setYesCount((preYesCount) => preYesCount + 1);
+      setYes(true);
+    }
+  };
+  return (
+    <>
+      <div className="helpful-1">
+        <Helpful
+          helpfulness={yesCount}
+          size={1}
+          handleOnClick={handleOnClick}
+        />
+      </div>
+      <StyledSeperator />
+      <AddAnswer />
+    </>
+  );
+};
 
 export default QuestionInfo;
