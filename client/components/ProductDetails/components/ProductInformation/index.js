26b5e831dc5c835ff23a17cb67ae8033ff3e2f19
Michael <kllmmc23@gmail.com>
2021-03-04 11:28:52 -0700

    create bff

--- client/components/ProductDetails/components/ProductInformation/index.js
+++ client/components/ProductDetails/components/ProductInformation/index.js
@@ -1,22 +1,32 @@
+import { shape } from 'prop-types';
 import React from 'react';
-import WithMargins from '../../../../hoc/with-margins';
+
 import { fontSizeBigger, fontSizeSmaller } from '../../../../ui/ui-fonts';
 import LinkTag from '../../../LinkTag';
 import Price from '../../../Price';
+import ProductCategoryStyles from './styles';
 import ProductTitle from '../../../Title';
 import StarRating from '../../../StarRating';
-import ProductCategoryStyles from './styles';
+import WithMargins from '../../../../hoc/with-margins';
 
-const ProducInformation = () => (
+const ProducInformation = ({product}) => product && (
   <>
     <StarRating />
     <WithMargins margin="1rem">
       <LinkTag size={fontSizeSmaller}>Read all reviews</LinkTag>
     </WithMargins>
-    <ProductCategoryStyles>category</ProductCategoryStyles>
-    <ProductTitle size={fontSizeBigger}>Expanded Product Name</ProductTitle>
-    <Price price={369} />
+    <ProductCategoryStyles>{product.productInformation.category}</ProductCategoryStyles>
+    <ProductTitle size={fontSizeBigger}>{product.productInformation.name}</ProductTitle>
+    <Price price={product.productStyles.results[0].original_price * 1} />
   </>
 );
+
+ProducInformation.propTypes = {
+  product: shape({}),
+};
+
+ProducInformation.defaultProps = {
+  product: null,
+};
 
 export default ProducInformation;
