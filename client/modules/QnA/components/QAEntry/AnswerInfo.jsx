ff2fcc46bfc37ceaa3e5827b5f5c250783e186cd
Kevin Liu <kevinalexliu@gmail.com>
2021-03-03 17:03:41 -0800

    functionality on answer helpful

--- client/modules/QnA/components/QAEntry/AnswerInfo.jsx
+++ client/modules/QnA/components/QAEntry/AnswerInfo.jsx
@@ -1,29 +1,40 @@
 /* eslint-disable import/extensions */
-import React from 'react';
+import React, { useState } from 'react';
 import NameDate from '../../../../components/NameDate';
 import Helpful from '../../../../components/Helpful';
 import Report from './Report.jsx';
 import { StyledSeperator } from './styles.js';
 
-const AnswerInfo = ({ question, handleOnClick }) => (
-  <div className="info">
-    <NameDate
-      name={question.answers[68].answerer_name}
-      date={question.answers[68].date}
-      string="By"
-    />
-    <StyledSeperator />
-    <div className="helpful-2">
-      <Helpful
-        helpfulness={question.answers[68].helpfulness}
-        size={2}
-        onClick={handleOnClick}
-        href={null}
+const AnswerInfo = ({ answer }) => {
+  const count = answer.helpfulness;
+  const [yesCount, setYesCount] = useState(count);
+  const [yes, setYes] = useState(false);
+  const handleOnClick = (e) => {
+    e.preventDefault();
+    if (!yes) {
+      setYesCount((preYesCount) => preYesCount + 1);
+      setYes(true);
+    }
+  }
+  return (
+    <div className="info">
+      <NameDate
+        name={answer.answerer_name}
+        date={answer.date}
+        string="By"
       />
+      <StyledSeperator />
+      <div className="helpful-2">
+        <Helpful
+          helpfulness={yesCount}
+          size={2}
+          handleOnClick={handleOnClick}
+        />
+      </div>
+      <StyledSeperator />
+      <Report />
     </div>
-    <StyledSeperator />
-    <Report />
-  </div>
-);
+  );
+};
 
 export default AnswerInfo;
