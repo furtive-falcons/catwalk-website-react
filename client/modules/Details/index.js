c789038cb58630ac57ca032561d2014cf164dd36
Michael Elliott <kllmmc23@gmail.com>
2021-03-04 11:30:12 -0700

    create bff (#129)

--- client/modules/Details/index.js
+++ client/modules/Details/index.js
@@ -1,10 +1,16 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
 import ProductDetails from '../../components/ProductDetails';
 
-const DetailPage = () => (
-  <>
-    <ProductDetails />
-  </>
-);
+const URL = 'http://localhost:3001/api/products/14931';
+
+const DetailPage = () => {
+  const [product, setProduct] = useState(null);
+  useEffect(() => {
+    axios.get(URL).then(response => setProduct(response.data.data));
+  }, []);
+
+  return <ProductDetails product={product} />;
+};
 
 export default DetailPage;
