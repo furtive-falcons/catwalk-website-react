e39065493880ec21d970c660145a59bc886519d8
Kevin Liu <kevinalexliu@gmail.com>
2021-03-04 10:38:21 -0800

    search filter

--- client/modules/QnA/index.js
+++ client/modules/QnA/index.js
@@ -1,5 +1,5 @@
 /* eslint-disable import/extensions */
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import SearchBar from './components/SearchBar.jsx';
 import Title from './components/Title.jsx';
 import EntryContainer from './components/EntryContainer.jsx';
@@ -8,14 +8,36 @@ import AddQuestion from './components/AddQuestion.jsx';
 import { Container } from './styles.js';
 import data from './data.js';
 
-const QnA = () => (
-  <Container>
-    <Title fontSize="1.6rem" title="QUESTIONS & ANSWERS" />
-    <SearchBar />
-    <EntryContainer questions={data.results} />
-    <MoreQuestion />
-    <AddQuestion />
-  </Container>
-);
+const QnA = () => {
+  // const [questions, setQuestions] = useState([]);
+  const [search, setSearch] = useState('');
+  const [filter, setFilter] = useState(data.results);
+
+  const filterQuestions = (question) => {
+    const qn = question.question_body.toLowerCase();
+    return qn.includes(search);
+  };
+
+  const handleSearch = (input) => {
+    setSearch(input.toLowerCase());
+    if (search.length >= 3) {
+      const qns = data.results; // change data to questions
+      const filtered = qns.filter(filterQuestions);
+      setFilter(filtered);
+    }
+  };
+
+  return (
+    <Container>
+      <Title fontSize="1.6rem" title="QUESTIONS & ANSWERS" />
+      <SearchBar search={handleSearch} />
+      <EntryContainer
+        questions={filter}
+      />
+      <MoreQuestion />
+      <AddQuestion />
+    </Container>
+  );
+};
 
 export default QnA;
