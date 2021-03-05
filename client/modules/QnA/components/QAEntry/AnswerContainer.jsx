e54729371edafa4fb3fbc39626c14b8c87b3bc80
Kevin Liu <kevinalexliu@gmail.com>
2021-03-05 10:43:33 -0800

    remove comment

--- client/modules/QnA/components/QAEntry/AnswerContainer.jsx
+++ client/modules/QnA/components/QAEntry/AnswerContainer.jsx
@@ -3,28 +3,20 @@ import React from 'react';
 import Answer from './Answer.jsx';
 import AnswerInfo from './AnswerInfo.jsx';
 
-const AnswerContainer = ({ display, answers, searched }) => {
-  // const filterAnswers = () => {
-  //   if (searched) {
-  //     const match = answers.filter;
-  //   }
-  // };
-  return (
-    <div className="answers">
-      {display.map((an, index) => (
-        <div key={an.id}>
-          <Answer
-            key={index}
-            answer={an}
-          />
-          <AnswerInfo
-            answer={an}
-          />
-          <br />
-        </div>
-      ))}
-    </div>
-  );
-};
-
+const AnswerContainer = ({ display }) => (
+  <div className="answers">
+    {display.map((an, index) => (
+      <div key={an.id}>
+        <Answer
+          key={index}
+          answer={an}
+        />
+        <AnswerInfo
+          answer={an}
+        />
+        <br />
+      </div>
+    ))}
+  </div>
+);
 export default AnswerContainer;
