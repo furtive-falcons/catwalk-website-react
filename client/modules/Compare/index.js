225d11059ea9005d79a890443ab4f106bed2a3cd
Shay Rosner <shayrosner@Shays-Mac-mini.local>
2021-03-06 00:07:25 -0800

    setup related product controller in server with ajax call from client for res.data

--- client/modules/Compare/index.js
+++ client/modules/Compare/index.js
@@ -5,57 +5,26 @@ import Carousel from './components/Carousel/Carousel';
 // import CarouselContainer from './components/Carousel/styles';
 // client/modules/Compare/components/Carousel/styles.js
 
-// const CompareCarousels = () => (
-//   <div data-test="component-compare">
-//     <Carousel />
-//     <Carousel />
-//   </div>
-// );
-
-// const URL = 'http://localhost:3001/api/products';
-
-// const CompareCarousels = ({ productId }) => {
-//   const [relatedIds, setRelatedIds] = React.useState(null);
-//   React.useEffect(() => {
-//     axios.get(`${URL}/${productId}/related`)
-//       .then((response) => {
-//         setRelatedIds(response.data.data);
-//       });
-//   }, []);
-
-//   return (
-//     // <div data-test="component-compare">
-//     <div>
-//       <Carousel relatedIds={relatedIds} />
-//       <Carousel />
-//     </div>
-//   );
-// };
-
 class CompareCarousels extends React.Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
     this.state = {
-      relatedIds: [],
+      relatedProducts: [],
     };
-    this.relatedProducts = this.relatedProducts.bind(this);
+    this.getRelatedProducts = this.getRelatedProducts.bind(this);
   }
 
   // getRelatedProducts(14034) = [14038, 14042, 14040, 14035, 14034]
-  // getRelatedProducts(14035) = [14034, 14035, 14036, 14037, 14038, 14039, 14040, 14041, 14042]
-  // getRelatedProducts(14036) = [14035, 14036, 14041, 14040]
-  // getRelatedProducts(14040) = [14035, 14036, 14037, 14040, 14042, 14043]
   componentDidMount() {
-    // this.setState({});
-    console.log('relatedProducts1', this.relatedProducts(14034));
+    this.getRelatedProducts(this.props.productId);
   }
 
-  relatedProducts(productId) {
+  getRelatedProducts(productId) {
     const url = 'http://localhost:3001/api/products';
     axios.get(`${url}/${productId}/related`)
       .then((res) => {
-        // console.log('res', res);
-        this.setState({relatedIds: res.data});
+        console.log('res', res.data);
+        this.setState({relatedProducts: res.data});
       }, (err) => {
         console.log(err);
       });
--- src/controllers/relatedController.js
+++ src/controllers/relatedController.js
@@ -3,62 +3,61 @@ const axios = require('axios');
 exports.getRelatedProducts = async (req, res) => {
   try {
     const URL = `${process.env.API_URL}/products/${req.params.id}/related`;
-    const { data: relatedProducts } = await axios(URL, {
+    const { data: relatedProductsArray } = await axios(URL, {
       headers: {
         Authorization: process.env.API_KEY,
       },
     });
     // [1, 2, 3]
-    // var sum = function(x + 1)
+    // getId[i] = relatedProductsArray[i]
     // relatedProducts.forEach((id) => {});
-    // let getId = {};
-    // for (let i = 0; i < relatedProducts.length; i++) {
-    //   getId[i] = sum(relatedProducts[i]);
-    //   const firstID = `${process.env.API_URL}/products/${relatedProducts[i]}`;
-    //   const { data: firstProduct } = await axios(firstID, {
-    //     headers: {
-    //       Authorization: process.env.API_KEY,
-    //     },
-    //   });
-    // }
+    // let relatedProducts = {14034: {relatedProduct: {defaultStyle: {styleObj: 1}}}};
 
-    const firstID = `${process.env.API_URL}/products/${relatedProducts[0]}`;
-    const { data: firstProduct } = await axios(firstID, {
-      headers: {
-        Authorization: process.env.API_KEY,
-      },
-    });
+    let relatedProducts = {};
+    for (let i = 0; i < relatedProductsArray.length; i++) {
+      // ex: /products/14034
+      const allID = `${process.env.API_URL}/products/${relatedProductsArray[i]}`;
+      const { data: relatedProduct} = await axios(allID, {
+        headers: {
+          Authorization: process.env.API_KEY,
+        },
+      });
+      relatedProducts[relatedProductsArray[i]] = relatedProduct;
 
-    const secID = `${process.env.API_URL}/products/${relatedProducts[1]}`;
-    const { data: secProduct } = await axios(secID, {
-      headers: {
-        Authorization: process.env.API_KEY,
-      },
-    });
+      // ex: /products/14034/styles
+      const allStyles = `${process.env.API_URL}/products/${relatedProductsArray[i]}`;
+      const { data: relatedStyles } = await axios(`${allStyles}/styles`, {
+        headers: {
+          Authorization: process.env.API_KEY,
+        },
+      });
+      
+      // if(relatedStyles.results["default?"]) {
+      //   relatedProduct.defaultStyle = relatedStyles.results;
+      // }
 
-    const thirdID = `${process.env.API_URL}/products/${relatedProducts[2]}`;
-    const { data: thirdProduct } = await axios(thirdID, {
-      headers: {
-        Authorization: process.env.API_KEY,
-      },
-    });
+      relatedProduct.defaultStyle = relatedStyles.results[0];
 
-    const fourthID = `${process.env.API_URL}/products/${relatedProducts[3]}`;
-    const { data: fourthProduct } = await axios(fourthID, {
-      headers: {
-        Authorization: process.env.API_KEY,
-      },
-    });
+      // ex: /reviews/meta?product_id=14034
+      const allRelatedRating = `${process.env.API_URL}`;
+      const { data: relatedRatingAverage } = await axios(`${allRelatedRating}/reviews/meta?product_id=${relatedProductsArray[i]}`, {
+        headers: {
+          Authorization: process.env.API_KEY,
+        },
+      });
+  
+      const { ratings } = relatedRatingAverage;
+  
+      const values = Object.values(ratings);
+  
+      const ratingAverage = values.reduce((acc, item) => item * 1 + acc, 0) / 5;
+
+      relatedProduct.ratingAverage = ratingAverage;
+    }
 
     res.status(200).json({
       status: 'success',
-      data: {
-        relatedProducts,
-        firstProduct,
-        secProduct,
-        thirdProduct,
-        fourthProduct,
-      },
+      data: Object.values(relatedProducts),
     });
   } catch (error) {
     res.status(500).json({
