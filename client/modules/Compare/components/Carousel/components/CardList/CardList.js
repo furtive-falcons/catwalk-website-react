88de0a989a32d321bd00fd54ef6b8b929d53bb53
Shay Rosner <shaymrosner@gmail.com>
2021-03-12 23:29:53 -0800

    code refactor and added prop types

--- client/modules/Compare/components/Carousel/components/CardList/CardList.js
+++ client/modules/Compare/components/Carousel/components/CardList/CardList.js
@@ -1,91 +1,77 @@
 import React, { useState, lazy, Suspense } from 'react';
-import { arrayOf, oneOfType, object } from 'prop-types';
-// import CardAssets from './components/CardAssets/CardAssets';
-// import CardDetails from './components/CardDetails/CardDetails';
+import {
+  arrayOf, string, number, exact,
+} from 'prop-types';
 import { CardListContainer, CardContainer, ButtonContainer } from './styles';
-// import Button from '../../../../../../components/Button'
+
 const CardAssets = lazy(() => import('./components/CardAssets/CardAssets'));
 const CardDetails = lazy(() => import('./components/CardDetails/CardDetails'));
 
 const CardList = ({ cardList }) => {
   const [index, setIndex] = useState(0);
-  if (!Array.isArray(cardList.data) || cardList.data.length <= 0) {
+  if (!Array.isArray(cardList) || cardList.length <= 0) {
     return null;
   }
 
-  const { length } = cardList.data;
-
-  const canGoLeft = index > 0;
+  const { length } = cardList;
 
   const goLeft = () => {
-    if (canGoLeft) {
+    if (index > 0) {
       setIndex(index - 1);
     }
   };
 
-  const canGoRight = index < length - 5;
-
   const goRight = () => {
-    if (canGoRight) {
+    if (index < length - 5) {
       setIndex(index + 1);
     }
   };
 
-  const cardListPage = cardList.data.slice(index, index + 5);
-  // id="component-cardlist", id="carousel-buttons"
+  const cardListPage = cardList.slice(index, index + 5);
   return (
-    <div data-test="component-cardlist">
-      <CardListContainer>
-        <Suspense fallback={<div>Loading...</div>}>
-          <ButtonContainer>
-            {canGoLeft ? (
-              <button className="left-prev-button" type="button" label="left" onClick={goLeft} data-test="component-button">
-                <i className="fas fa-chevron-left" />
-              </button>
-            ) : null}
-            {canGoRight ? (
-              <button className="right-prev-button" type="button" lable="right" onClick={goRight} data-test="component-button">
-                <i className="fas fa-chevron-right" />
-              </button>
-            ) : null}
-
-          </ButtonContainer>
-          <div id="component-cardlist">
-            {cardListPage.map((card) => (
-              <CardContainer key={`container_${card.id}`}>
-                <CardAssets card={card} key={`assets_${card.id}`} />
-                <CardDetails card={card} key={`details_${card.id}`} />
-              </CardContainer>
-            ))}
-          </div>
-        </Suspense>
-      </CardListContainer>
-    </div>
+    <CardListContainer data-test="component-cardlist">
+      <Suspense fallback={<div>Loading...</div>}>
+        <ButtonContainer>
+          {index > 0 ? (
+            <button className="left-prev-button" type="button" label="left" onClick={goLeft}>
+              <i className="fas fa-chevron-left" />
+            </button>
+          ) : null}
+          {index < length - 5 ? (
+            <button className="right-prev-button" type="button" lable="right" onClick={goRight}>
+              <i className="fas fa-chevron-right" />
+            </button>
+          ) : null}
+        </ButtonContainer>
+        {cardListPage.map((card) => (
+          <CardContainer key={`container_${card.id}`}>
+            <CardAssets card={card} key={`assets_${card.id}`} />
+            <CardDetails card={card} key={`details_${card.id}`} />
+          </CardContainer>
+        ))}
+      </Suspense>
+    </CardListContainer>
   );
 };
 
 CardList.propTypes = {
-  cardList: arrayOf(oneOfType([object])),
+  cardList: arrayOf(exact({
+    id: number,
+    name: string,
+    category: string,
+    features: arrayOf(exact({
+      feature: string,
+      value: string,
+    })),
+    original_price: string,
+    sale_price: string,
+    thumbnail_url: string,
+    ratingAverage: number,
+  })),
 };
 
 CardList.defaultProps = {
-  cardList: [],
+  cardList: null,
 };
 
 export default CardList;
-
-/*
-{canGoLeft
-            ? <Button className="left-prev-button"
-            handleOnClick={goLeft} primary
-            icon="chevron-left">
-          </Button>
-          : null}
-
-          {canGoRight
-            ? <Button className="right-prev-button"
-            handleOnClick={goRight}
-            primary icon="chevron-right">
-          </Button>
-          : null}
-*/
