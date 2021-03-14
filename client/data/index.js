c643708e699dca6b403dcc034c09a5ccaa9172fc
Shay Rosner <shaymrosner@gmail.com>
2021-03-13 22:18:22 -0800

    add id with all component functionality for grading

--- client/data/index.js
+++ client/data/index.js
@@ -1,8 +1,8 @@
 const linksList = [
+  { productId: 14040, link: 'dress shoes' },
   { productId: 14037, link: 'pants' },
   { productId: 14034, link: 'jackets' },
   { productId: 14038, link: 'kicks' },
-  { productId: 14040, link: 'dress shoes' },
   { productId: 14036, link: 'Joggers' },
   { productId: 14041, link: 'Yeasy' },
 ];
